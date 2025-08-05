75d362c96caff6c7f359552713616879bbc2c31e
elchubi <david.bravo.g@gmail.com>
2025-08-05 23:28:21 +0000

    Add Tailwind setup and initial files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{html,js,ts,tsx}",
+    "./components/**/*.{html,js,ts,tsx}",
+    "./src/**/*.{html,js,ts,tsx}",
+    "./public/index.html",
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
